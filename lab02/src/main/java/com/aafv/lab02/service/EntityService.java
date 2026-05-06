49aa29716d65e825a713300d0eba1bb15db4da88
AlejandroFV74 <00067323@uca.edu.sv>
2026-05-06 14:42:10 -0600

    feat: CRUD interfaces methods added

--- lab02/src/main/java/com/aafv/lab02/service/EntityService.java
+++ lab02/src/main/java/com/aafv/lab02/service/EntityService.java
@@ -2,6 +2,17 @@ package com.aafv.lab02.service;
 
 import com.aafv.lab02.domain.entity.EntityC;
 
+import java.util.UUID;
+
 public interface EntityService {
+   //Post
     void createEntity(EntityC entity);
+    //Get
+    EntityC getAllEntities();
+    EntityC getEntityByPatronus(String patronus);
+    EntityC getOnlyDeatheaters();
+    //Update
+    void updateEntity(EntityC entity, UUID uuid);
+    //Delete
+    EntityC deleteEntity(UUID uuid);
 }
