118490dffb472eb241551152fe7fa6ae052d7086
AlejandroFV74 <00067323@uca.edu.sv>
2026-05-06 13:57:23 -0600

    feat: service logic added

--- lab02/src/main/java/com/aafv/lab02/service/EntityService.java
+++ lab02/src/main/java/com/aafv/lab02/service/EntityService.java
@@ -1,4 +1,7 @@
 package com.aafv.lab02.service;
 
+import com.aafv.lab02.domain.entity.EntityC;
+
 public interface EntityService {
+    void createEntity(EntityC entity);
 }
--- lab02/src/main/java/com/aafv/lab02/service/imp/EntityServiceImpl.java
+++ lab02/src/main/java/com/aafv/lab02/service/imp/EntityServiceImpl.java
@@ -1,4 +1,20 @@
 package com.aafv.lab02.service.imp;
 
-public class EntityServiceImpl {
+import com.aafv.lab02.domain.entity.EntityC;
+import com.aafv.lab02.repository.EntityRepository;
+import com.aafv.lab02.service.EntityService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EntityServiceImpl implements EntityService {
+
+    private final EntityRepository entityRepository;
+
+    @Override
+    public void createEntity(EntityC entity) {
+        entityRepository.save(entity);
+    }
+
 }
