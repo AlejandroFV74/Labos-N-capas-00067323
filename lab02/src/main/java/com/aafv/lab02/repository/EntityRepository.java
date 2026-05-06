b8a3bce1ebb44c0bc674a593640dd5c5581f9908
AlejandroFV74 <00067323@uca.edu.sv>
2026-05-06 13:52:37 -0600

    feat: repository function implemented

--- lab02/src/main/java/com/aafv/lab02/repository/EntityRepository.java
+++ lab02/src/main/java/com/aafv/lab02/repository/EntityRepository.java
@@ -1,4 +1,10 @@
 package com.aafv.lab02.repository;
 
-public interface EntityRepository {
+import com.aafv.lab02.domain.entity.EntityC;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface EntityRepository extends JpaRepository<EntityC, UUID> {
+
 }
