9d39bbf5ddd3042cfd154ff862cef6e14e6d9dc4
AlejandroFV74 <00067323@uca.edu.sv>
2026-05-20 16:17:55 -0600

    perf: new exceptions at global handler

--- lab03/src/main/java/com/aafv/lab03/exceptions/GlobalExceptionHandler.java
+++ lab03/src/main/java/com/aafv/lab03/exceptions/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ package com.aafv.lab03.exceptions;
 import com.aafv.lab03.domain.dto.response.ApiErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -15,6 +16,17 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ApiErrorResponse> handleResourceNotFound(ResourceNotFoundException e) {
         return buildErrorResponse(HttpStatus.NOT_FOUND, e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiErrorResponse> handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
+        String message = e.getBindingResult().getFieldError().getDefaultMessage();
+        return buildErrorResponse(HttpStatus.BAD_REQUEST, message);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiErrorResponse> handleException(Exception e) {
+        return buildErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
     }
 
     public ResponseEntity<ApiErrorResponse> buildErrorResponse(HttpStatus status, Object data) {
