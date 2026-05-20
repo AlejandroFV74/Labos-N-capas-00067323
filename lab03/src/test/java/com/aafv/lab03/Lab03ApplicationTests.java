6a5925ecae8a5eb5718fb03d76055be87b76f29e
AlejandroFV74 <00067323@uca.edu.sv>
2026-05-20 11:04:52 -0600

    feat: lab03 Spring Boot set up

---
+++ lab03/src/test/java/com/aafv/lab03/Lab03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aafv.lab03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
